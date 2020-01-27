d80f984da2a6d75ccb8150f8b5e61f72fbb4c799
Ruben Schmidmeister <ruben.schmidmeister@icloud.com>
2020-01-27 14:10:43 +0100

    Use macro to generate transformation switch

--- src/lib.rs
+++ src/lib.rs
@@ -79,45 +79,52 @@ trait Transformation {
     fn map_digit(digit: u8) -> char;
 }
 
-struct Superscript;
-impl Transformation for Superscript {
-    const MINUS_CHARACTER: char = '\u{207B}';
-    fn map_digit(digit: u8) -> char {
-        match digit {
-            0 => '\u{2070}',
-            1 => '\u{00B9}',
-            2 => '\u{00B2}',
-            3 => '\u{00B3}',
-            4 => '\u{2074}',
-            5 => '\u{2075}',
-            6 => '\u{2076}',
-            7 => '\u{2077}',
-            8 => '\u{2078}',
-            9 => '\u{2079}',
-            _ => unreachable!(),
+macro_rules! transformation {
+    ($name: ident { '-' => $minus_character: expr, $($digit: expr => $character: expr,)* }) => {
+        struct $name;
+        impl Transformation for $name {
+            const MINUS_CHARACTER: char = $minus_character;
+            fn map_digit(digit: u8) -> char {
+                match digit {
+                    $($digit => $character,)*
+                    _ => unreachable!(),
+                }
+            }
         }
-    }
+    };
 }
 
-struct Subscript;
-impl Transformation for Subscript {
-    const MINUS_CHARACTER: char = '\u{208B}';
-    fn map_digit(digit: u8) -> char {
-        match digit {
-            0 => '\u{2080}',
-            1 => '\u{2081}',
-            2 => '\u{2082}',
-            3 => '\u{2083}',
-            4 => '\u{2084}',
-            5 => '\u{2085}',
-            6 => '\u{2086}',
-            7 => '\u{2087}',
-            8 => '\u{2088}',
-            9 => '\u{2089}',
-            _ => unreachable!(),
-        }
+transformation!(
+    Superscript {
+        '-' => '\u{207B}',
+        0 => '\u{2070}',
+        1 => '\u{00B9}',
+        2 => '\u{00B2}',
+        3 => '\u{00B3}',
+        4 => '\u{2074}',
+        5 => '\u{2075}',
+        6 => '\u{2076}',
+        7 => '\u{2077}',
+        8 => '\u{2078}',
+        9 => '\u{2079}',
     }
-}
+);
+
+transformation!(
+    Subscript {
+        '-' => '\u{208B}',
+        0 => '\u{2080}',
+        1 => '\u{2081}',
+        2 => '\u{2082}',
+        3 => '\u{2083}',
+        4 => '\u{2084}',
+        5 => '\u{2085}',
+        6 => '\u{2086}',
+        7 => '\u{2087}',
+        8 => '\u{2088}',
+        9 => '\u{2089}',
+    }
+);
 
 fn find_special_vulgar_fraction(nominator: i64, denominator: i64) -> Option<char> {
     let index = SPECIAL_FRACTIONS
