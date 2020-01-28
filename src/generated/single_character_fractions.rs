bb320d4a96f10f3f75438acff1b7519825d469c9
Ruben Schmidmeister <ruben.schmidmeister@icloud.com>
2020-01-28 19:04:49 +0100

    Update generated file

--- src/generated/single_character_fractions.rs
+++ src/generated/single_character_fractions.rs
@@ -2,25 +2,25 @@
 // ./scripts/update-generated-code.sh
 pub(crate) fn find_single_character_fraction(nominator: i64, denominator: i64) -> Option<char> {
     match (nominator, denominator) {
-        (0i64, 3i64) => Some('\u{2189}'),
-        (1i64, 2i64) => Some('\u{bd}'),
-        (1i64, 3i64) => Some('\u{2153}'),
         (1i64, 4i64) => Some('\u{bc}'),
-        (1i64, 5i64) => Some('\u{2155}'),
-        (1i64, 6i64) => Some('\u{2159}'),
+        (1i64, 2i64) => Some('\u{bd}'),
+        (3i64, 4i64) => Some('\u{be}'),
         (1i64, 7i64) => Some('\u{2150}'),
-        (1i64, 8i64) => Some('\u{215b}'),
         (1i64, 9i64) => Some('\u{2151}'),
         (1i64, 10i64) => Some('\u{2152}'),
+        (1i64, 3i64) => Some('\u{2153}'),
         (2i64, 3i64) => Some('\u{2154}'),
+        (1i64, 5i64) => Some('\u{2155}'),
         (2i64, 5i64) => Some('\u{2156}'),
-        (3i64, 4i64) => Some('\u{be}'),
         (3i64, 5i64) => Some('\u{2157}'),
-        (3i64, 8i64) => Some('\u{215c}'),
         (4i64, 5i64) => Some('\u{2158}'),
+        (1i64, 6i64) => Some('\u{2159}'),
         (5i64, 6i64) => Some('\u{215a}'),
+        (1i64, 8i64) => Some('\u{215b}'),
+        (3i64, 8i64) => Some('\u{215c}'),
         (5i64, 8i64) => Some('\u{215d}'),
         (7i64, 8i64) => Some('\u{215e}'),
+        (0i64, 3i64) => Some('\u{2189}'),
         _ => None,
     }
 }
