6076af31d2687320458bb984d3780d1add9e0ba7
Ruben Schmidmeister <ruben.schmidmeister@icloud.com>
2020-03-04 20:44:56 +0100

    Mark function as unsafe

--- crates/wasm/src/lib.rs
+++ crates/wasm/src/lib.rs
@@ -6,7 +6,7 @@ use vulgar_fractions::VulgarFraction;
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
 #[no_mangle]
-pub extern "C" fn vulgar_fraction(nominator: i32, denominator: i32) -> *mut c_char {
+pub unsafe extern "C" fn vulgar_fraction(nominator: i32, denominator: i32) -> *mut c_char {
     let vulgar_fraction =
         VulgarFraction::new(i64::from(nominator), i64::from(denominator)).to_string();
     let c_string = CString::new(vulgar_fraction).expect("Fraction should not contain null bytes");
@@ -14,18 +14,16 @@ pub extern "C" fn vulgar_fraction(nominator: i32, denominator: i32) -> *mut c_ch
 }
 
 #[no_mangle]
-pub extern "C" fn vulgar_fraction_len(raw: *mut c_char) -> usize {
-    let string = unsafe { CString::from_raw(raw) };
+pub unsafe extern "C" fn vulgar_fraction_len(raw: *mut c_char) -> usize {
+    let string = CString::from_raw(raw);
     let length = string.as_bytes().len();
     string.into_raw();
     length
 }
 
 #[no_mangle]
-pub extern "C" fn vulgar_fraction_free(raw: *mut c_char) {
+pub unsafe extern "C" fn vulgar_fraction_free(raw: *mut c_char) {
     if !raw.is_null() {
-        unsafe {
-            CString::from_raw(raw);
-        }
+        CString::from_raw(raw);
     }
 }
