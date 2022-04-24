70f035d65ac0094555040678dddf9c653f24e6ad
Ruben Schmidmeister <4602612+bash@users.noreply.github.com>
2022-04-24 18:39:47 +0200

    Fix URL for wasm file

--- web/public/js/wasm-loading.js
+++ web/public/js/wasm-loading.js
@@ -1,7 +1,7 @@
 export async function loadWasm() {
     const memory = new WebAssembly.Memory({ initial: 64 });
     const imports = { env: { memory } };
-    const resp = await fetch('/vulgar_fractions.wasm');
+    const resp = await fetch('vulgar_fractions.wasm');
     const bytes = await resp.arrayBuffer();
     const source = await WebAssembly.instantiate(bytes, imports);
     return { exports: source.instance.exports, memory };
